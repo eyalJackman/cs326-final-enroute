a391a8f6adee1e31cdfa585cd4409ccdb0f540e2
eyalJackman <eyal.jackman@gmail.com>
2022-04-19 19:55:24 -0700

    Image added

--- mockDatabase.js
+++ mockDatabase.js
@@ -1,11 +1,14 @@
 const data = [{
         name: "Boston",
+        image: "cs326_destination_boston.jpg",
     },
     {
         name: "Punta Cana",
+        image: "cs326_destination_punta_cana.jpg",
     },
     {
-        name: "Sao Paulo",
+        name: "Thessaloniki",
+        image: "cs326_destination_thessaloniki.jpg",
     },
 ];
 
