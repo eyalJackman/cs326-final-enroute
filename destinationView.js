a13545a70ed19b816707e2aec404288da7bc40ee
eyalJackman <eyal.jackman@gmail.com>
2022-04-19 18:52:55 -0700

    API Fetch properly working

--- destinationView.js
+++ destinationView.js
@@ -10,31 +10,42 @@ const url =
  * @description This will render the first 100 words and an image of whatever destination is said to it. This will be sent to desination.html
  */
 const addDestination = async(destination) => {
-    const correctFormat = destination.split(" ").join("%20");
-    const url = `https://en.wikipedia.org/w/api.php?origin=*&action=query&titles=${correctFormat}`;
-    const response = await fetch(url, {
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-        },
-    });
-    const pageId = (curid) =>
-        `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=${curid}`;
-    let id;
+    const correctFormat = destination.split(" ").join("%20"); //Boston
+    // const url = `https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&titles=${correctFormat}`;
+    // const url = `https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&titles=${correctFormat}&prop=extracts`;
+    // const url = `https: //en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${correctFormat}&limit=5`;
+    const url =
+        // `https: //en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=${curid}`;
+        `https://en.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${correctFormat}`;
+    const response = await fetch(url);
+
+    let extract;
+    await response
+        .json()
+        .then((obj) => {
+            const pages = obj["query"]["pages"];
+            for (entry in pages) {
+                extract = pages[entry]["extract"];
+            }
+        })
+        .then(
+            (document.getElementById("information").innerHTML =
+                JSON.stringify(extract))
+        );
+    // let extract;
+    // for (const entry of pages) {
+    //     extract = pages[entry]["extract"];
+    // }
+    // let id;
     if (response.ok) {
-        id = await response.json();
-        const content = await fetch(pageId(id), {
-            method: "get",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        }).catch(console.log);
-        if (content.ok) {
-            document.getElementById("information").innerHTML =
-                content["query"]["pages"][id]["extract"];
-        }
-        // document.getElementById("information").innerHTML = JSON.stringify(data);
+        //     id = await response.json();
+        //     const content = await fetch(pageId(id));
+        //     if (content.ok) {
+        //         document.getElementById("information").innerHTML =
+        //             content["query"]["pages"][id]["extract"];
+        //     }
+        document.getElementById("information").innerHTML = JSON.stringify(extract);
     }
     // return "test";
 };
-// setTimeout(() => addDestination("Boston"), 1000);
+setTimeout(() => addDestination("Boston"), 1000);
