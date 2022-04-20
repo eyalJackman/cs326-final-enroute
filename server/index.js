644d99b56a3ba479f66ec48b5aa1a0dc9acb2804
Simona Zilberberg <simonazilber@umass.edu>
2022-04-19 22:26:33 -0400

    added server

---
+++ server/index.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import { readFile, writeFile } from 'fs/promises';
+import logger from 'morgan';
+
+const TEMP_FILTER_FILE = 'tempFilter.json';
+
+// Returns a function that will read a score file.
+function readFromFile(path) {
+  return async () => {
+    try {
+      const data = await readFile(path, 'utf8');
+      const response = JSON.parse(data);
+      return response;
+    } catch (error) {
+      // Likely the file doesn't exist
+      return [];
+    }
+  };
+}
+const readFilters = readFromFile(TEMP_FILTER_FILE);
+
+function saveToFilterFile(path) {
+  return async (region, season, weather, vacationType) => {
+    const data = { region, season, weather, vacationType};
+    const filters = await readFilters(path);
+    filters.push(data);
+    writeFile(path, JSON.stringify(scores), 'utf8');
+  };
+}
+const saveFilter = saveToFilterFile(TEMP_FILTER_FILE);
+
+// Create the Express app and set the port number.
+const app = express();
+const port = 3000;
+
+// Add Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Add the morgan middleware to the app.
+app.use(logger('dev'));
+// Add the express.static middleware to the app.
+app.use(express.static('public'));
+
+app.post('/saveFilter', (req, res) => {
+    const filter = req.body;
+    // saveFilter(filter.region,filter.season,filter.weather,filter.vacationType);
+    res.status(200).json({"status": "success"});
+});
+  
+  // Start the server on port 3000.
+  app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+  });
+
