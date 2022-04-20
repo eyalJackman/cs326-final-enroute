b7f719f5b9811ffd6a3fea88e8857c3629c9d63c
eyalJackman <eyal.jackman@gmail.com>
2022-04-19 22:00:45 -0700

    Added name

--- destinationView.js
+++ destinationView.js
@@ -1,8 +1,8 @@
 import data from "./mockDatabase.js";
 
-const information = document.getElementById("information");
+const information = document.getElementById("destination_information");
 const img = document.getElementById("destination_image");
-
+const name = document.getElementById("destination_name");
 /**
  *
  * @param {string} destination The destination that wants to be travelled to
@@ -10,23 +10,29 @@ const img = document.getElementById("destination_image");
  * @description This will render the first 100 words and an image of whatever destination is said to it. This will be sent to desination.html
  */
 const addDestination = async(destination) => {
-    const correctFormat = destination["name"].split(" ").join("%20"); //Boston
+    // Image
     const imgPath = `./images/${destination["image"]}`;
     img.setAttribute("src", imgPath);
+
+    // Description
+    const correctFormat = destination["name"].split(" ").join("%20"); //Boston
     const url = `https://en.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${correctFormat}`;
     const response = await fetch(url);
 
     let extract;
-    await response.json().then((obj) => {
-        const pages = obj["query"]["pages"];
-        for (const entry in pages) {
-            extract = pages[entry]["extract"];
-        }
-    });
-
+    await response
+        .json()
+        .then((obj) => {
+            const pages = obj["query"]["pages"];
+            for (const entry in pages) {
+                extract = pages[entry]["extract"];
+            }
+        })
+        .catch((err) => console.log(err));
     if (response.ok) {
         information.innerHTML = JSON.stringify(extract);
     }
+    name.appendChild(document.createTextNode(destination["name"]));
 };
 const randDest = data[Math.floor(Math.random() * data.length)];
 addDestination(randDest);
