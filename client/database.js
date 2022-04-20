7cf3bd6b0c74d167fa951f6878cca759b7da7723
DarshGondalia <dgondalia@umass.edu>
2022-04-20 17:41:28 -0400

    SO DAMN HARD TO REMOVE BUGS

---
+++ client/database.js
@@ -0,0 +1,18 @@
+//export class db
+//read, update, fetch, fetch with parameters functions
+
+export class Database{
+    //add to favorites method, will add the destination name to the users favorites table;
+    //fetch favorites method, will fetch the information of the destinations based 
+    // on the list of favorites in the partivular user's table
+    //login method, will check whether the user exists in the users table, 
+    //returns true or false (saved in a global variable 'login')
+    //signup method, adds the users email and password to the users table in the database
+    
+    //fetch search results method, "select * where region = region, season = season, 
+    //weather = weather, vacationtype = vacationtype from destinations table", 
+    //returns a list of objects with more information such as description, picture...
+
+    
+
+}
--- client/saveFilter.js
+++ client/saveFilter.js
@@ -1,4 +1,8 @@
+import { SearchResults } from "./searchResults.js";
+
 const searchButton = document.getElementById('search');
+
+
 
 searchButton.addEventListener('click', () => {
   const regionOption = document.getElementById('region');
@@ -12,7 +16,6 @@ searchButton.addEventListener('click', () => {
     let weather = weatherOption.value;
     let vacationType = vacationTypeOption.value;
     //let filterObj = {region: region, season: season, weather: weather, vacationType: vacationType};
-
     async function saveWordScore(region, season, weather, vacationType){
       fetch(`http://localhost:3000/saveFilter`, {
         method: "POST",
@@ -28,3 +31,25 @@ searchButton.addEventListener('click', () => {
     }
     postFilter();
   });
+
+  
+searchButton.addEventListener("click", ()=> {
+  //create class instance with region season weather vacation type paramaters
+    //rendersearch results using element id 'col-md-3 text-center' here?
+  const regionOption = document.getElementById('region');
+  const seasonOption = document.getElementById('season');
+  const weatherOption = document.getElementById('weather');
+  const vacationTypeOption = document.getElementById('vacation');
+  console.log("search button clicked");
+  let region = regionOption.value;
+  console.log(region);
+  let season = seasonOption.value;
+  let weather = weatherOption.value;
+  let vacationType = vacationTypeOption.value;
+
+  const search = new SearchResults();
+  render(document.getElementById("searchResults"));
+});
+
+// const favButton = document.getElementById("favorite");
+// favButton.addEventListener("click", addToFavorites());
