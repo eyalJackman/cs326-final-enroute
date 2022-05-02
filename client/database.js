8a1b672229939459d769c369974102031785e2d0
MananAbbott <manan.abbottt@gmail.com>
2022-05-02 16:06:40 -0400

    Functions for user database

--- client/database.js
+++ client/database.js
@@ -1,7 +1,70 @@
+import 'dotenv/config';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 //export class db
 //read, update, fetch, fetch with parameters functions
 
 export class Database{
+    constructor(dburl){
+        this.dburl = dburl;
+    }
+    async connect() {
+        this.client = await MongoClient.connect(this.dburl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverApi: ServerApiVersion.v1,
+        });
+        this.db = this.client.db('enroute');
+        await this.init();
+    }
+    async init(){
+        this.collection_users = this.db.collection('users');
+        this.collection_destinations = this.db.collection('destinations');
+    }
+    async close(){
+        this.client.close();
+    }
+    //add a new user
+    async createUser(id, password, name, email){
+        const res = await this.collection_users.insertOne({_id: id, password, name, email});
+        return res;
+    }
+    //read a user
+    async readUser(id){
+        const res = await this.collection_users.findOne({_id: id});
+    }
+    // update user name
+    async updateUserName(id, name){
+        const res = await this.collection_users.updateOne(
+            {_id: id }, {$set: {name}}
+        );
+        return res;
+    }
+    // update user email
+    async updateUserEmail(id, email){
+        const res = await this.collection_users.updateOne(
+            {_id: id }, {$set: {email}}
+        );
+        return res;
+    }
+    // update user password
+    async updateUserName(id, password){
+        const res = await this.collection_users.updateOne(
+            {_id: id }, {$set: {password}}
+        );
+        return res;
+    }
+    // delete a user
+    async deleteUser(id){
+        const res = await this.collection_users.deleteOne({_id: id});
+        return res;
+    }
+    // read all users
+    async readAllUsers(){
+        const res = await this.collection_users.find({}).toArray();
+        return res;
+    }
+    
+
     //add to favorites method, will add the destination name to the users favorites table;
     //fetch favorites method, will fetch the information of the destinations based 
     // on the list of favorites in the partivular user's table
