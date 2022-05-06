760cd877fcf1da410ae6193b6ca0a2bbe82c94db
Simona Zilberberg <simonazilber@umass.edu>
2022-05-06 00:48:06 -0400

    createUser is complete

--- client/signup.js
+++ client/signup.js
@@ -1,7 +1,32 @@
-document.getElementById('signup_register').addEventListener('click', () => {
-    console.log("button works");
-    let username = document.getElementById("signup_username").value;
-    let password = document.getElementById("signup_password").value;
-    console.log(username);
-    console.log(password);
+async function saveUser(user,password) {
+    const data = JSON.stringify({ user,password });
+    const response = await fetch('/createUser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data,
+    });
+    if (!response.ok) {
+      console.error(`Unable to save ${data} to server`);
+    }
+}
+
+let registerButton = document.getElementById('signup_register');
+
+registerButton.addEventListener('click', async () => {
+    let username = document.getElementById("signup_username");
+    let password = document.getElementById("signup_password");
+    if (username.value === "" || password.value === "") {
+        alert(`Please Add a Username and Password`);
+        return;
+    }
+    else{
+        await saveUser(username.value,password.value);
+        username.value = "";
+        password.value = "";
+        alert(`Your Account has been Created!`);
+        return;
+    }
+    
 });
--- server/index.js
+++ server/index.js
@@ -1,71 +1,121 @@
 import express from "express";
-import { readFile, writeFile } from "fs/promises";
-import logger from "morgan";
+import { Database } from './database.js';
 
-const TEMP_FILTER_FILE = "tempFilter.json";
+class Server{
+  constructor(dburl) {
+    this.dburl = dburl;
+    this.app = express();
+    this.app.use(express.json());
+    this.app.use('/', express.static('client'));
+  }
 
-// Returns a function that will read a score file.
-function readFromFile(path) {
-    return async() => {
-        try {
-            const data = await readFile(path, "utf8");
-            const response = JSON.parse(data);
-            return response;
-        } catch (error) {
-            // Likely the file doesn't exist
-            return [];
-        }
-    };
+  async initRoutes(){
+    const self = this;
+
+    this.app.post('/createUser', async (req, res) => {
+      try {
+        const { user,password } = req.body;
+        const account = await self.db.createUser(user, password);
+        res.send(JSON.stringify(account));
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+
+    // this.app.get('/highestWordScores', async (req, res) => {
+    //   try {
+    //     const highestWordScores = await self.db.top10WordScores();
+    //     res.send(JSON.stringify(highestWordScores));
+    //   } catch (err) {
+    //     res.status(500).send(err);
+    //   }
+    // });
+  
+  }
+
+  async initDb() {
+    this.db = new Database(this.dburl);
+    await this.db.connect();
+  }
+
+  async start() {
+    await this.initRoutes();
+    await this.initDb();
+    const port = process.env.PORT || 8080;
+    this.app.listen(port, () => {
+      console.log(`ScoreServer listening on port ${port}!`);
+    });
+  }
 }
-const readFilters = readFromFile(TEMP_FILTER_FILE);
 
-function saveToFilterFile(path) {
-    return async(region, season, weather, vacationType) => {
-        const data = { region, season, weather, vacationType };
-        const filters = await readFilters(path);
-        filters.push(data);
-        writeFile(path, JSON.stringify(filters), "utf8");
-    };
-}
-const saveFilter = saveToFilterFile(TEMP_FILTER_FILE);
+console.log(process.env.MONGODB_URI);
+const server = new Server(process.env.MONGODB_URI);
+server.start();
 
-// Create the Express app and set the port number.
-const app = express();
-const port = 3000;
+// const TEMP_FILTER_FILE = "tempFilter.json";
 
-// Add Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// // Returns a function that will read a score file.
+// function readFromFile(path) {
+//     return async() => {
+//         try {
+//             const data = await readFile(path, "utf8");
+//             const response = JSON.parse(data);
+//             return response;
+//         } catch (error) {
+//             // Likely the file doesn't exist
+//             return [];
+//         }
+//     };
+// }
+// const readFilters = readFromFile(TEMP_FILTER_FILE);
 
-//Add the morgan middleware to the app.
-app.use(logger("dev"));
+// function saveToFilterFile(path) {
+//     return async(region, season, weather, vacationType) => {
+//         const data = { region, season, weather, vacationType };
+//         const filters = await readFilters(path);
+//         filters.push(data);
+//         writeFile(path, JSON.stringify(filters), "utf8");
+//     };
+// }
+// const saveFilter = saveToFilterFile(TEMP_FILTER_FILE);
 
-// Add the express.static middleware to the app.
-app.use(express.static("client"));
+// // Create the Express app and set the port number.
+// const app = express();
+// const port = 3000;
 
-app.get("/", (req, res) => {
-    res.sendFile("client/home.html", { root: "./" });
-});
+// // Add Middleware
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 
-// app.get("/client/destination", (req, res) => {
-//     res.send("Test");
-//     // const params = req.params;
-//     // const { name } = params;
-//     // await destinationView.addDestination(name); (Changes destination.html and then uploads it to the DOM)
-//     // res.sendFile("../client/destination.html");
+// //Add the morgan middleware to the app.
+// app.use(logger("dev"));
+
+// // Add the express.static middleware to the app.
+// app.use(express.static("client"));
+
+// app.get("/", (req, res) => {
+//     res.sendFile("client/home.html", { root: "./" });
 // });
 
-app.post("/saveFilter", (req, res) => {
-    const filter = req.body;
-    saveFilter(filter.region, filter.season, filter.weather, filter.vacationType);
-    res.status(200).json({ status: "success" });
-});
+// // app.get("/client/destination", (req, res) => {
+// //     res.send("Test");
+// //     // const params = req.params;
+// //     // const { name } = params;
+// //     // await destinationView.addDestination(name); (Changes destination.html and then uploads it to the DOM)
+// //     // res.sendFile("../client/destination.html");
+// // });
 
-app.all("*", async(request, response) => {
-    response.status(404).send(`Not found: ${request.path}`);
-});
+// app.post("/saveFilter", (req, res) => {
+//     const filter = req.body;
+//     saveFilter(filter.region, filter.season, filter.weather, filter.vacationType);
+//     res.status(200).json({ status: "success" });
+// });
 
-// Start the server on port 3000.
-app.listen(process.env.PORT || port, () => {
-    console.log(`Server started on http://localhost:${port}`);
-});
+// app.all("*", async(request, response) => {
+//     response.status(404).send(`Not found: ${request.path}`);
+// });
+
+// // Start the server on port 3000.
+// app.listen(process.env.PORT || port, () => {
+//     console.log(`Server started on http://localhost:${port}`);
+// });
