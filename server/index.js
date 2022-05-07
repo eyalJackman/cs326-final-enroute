2cf8d4bd7d61cf6e461447f39b0277e4b22e9f5a
eyalJackman <eyal.jackman@gmail.com>
2022-05-06 19:00:58 -0700

    Server fix

--- server/index.js
+++ server/index.js
@@ -120,13 +120,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //Add the morgan middleware to the app.
-app.use(logger("dev"));
+// app.use(logger("dev"));
 
 // Add the express.static middleware to the app.
 app.use(express.static("client"));
 
 app.get("/", (req, res) => {
-  res.sendFile("client/home.html", { root: "./" });
+    res.sendFile("client/home.html", { root: "./" });
 });
 
 // app.get("/client/destination", (req, res) => {
@@ -148,11 +148,11 @@ app.get("/", (req, res) => {
 //   res.status(200).json({ status: "success" });
 // });
 
-app.all("*", async (request, response) => {
-  response.status(404).send(`Not found: ${request.path}`);
+app.all("*", async(request, response) => {
+    response.status(404).send(`Not found: ${request.path}`);
 });
 
 // Start the server on port 3000.
 app.listen(process.env.PORT || port, () => {
-  console.log(`Server started on http://localhost:${port}`);
+    console.log(`Server started on http://localhost:${port}`);
 });
