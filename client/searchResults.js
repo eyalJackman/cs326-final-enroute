ffbcedc605ffa92c4c9bf60d25295493e154f894
eyalJackman <eyal.jackman@gmail.com>
2022-05-06 17:19:43 -0700

    Added destinations to search results

--- client/searchResults.js
+++ client/searchResults.js
@@ -1,32 +1,32 @@
 import { Database } from "./database.js";
 
-export class SearchResults{
+export class SearchResults {
     //create class search results
-//constructor accept 4 parameters, region, season, weather, vacationtype
-//iterate through the list of destinations or call function from database.js and render the elements
-// render: for loop with 10 iterations, create div (card) in classlist card, append child to colmd3textcenter
+    //constructor accept 4 parameters, region, season, weather, vacationtype
+    //iterate through the list of destinations or call function from database.js and render the elements
+    // render: for loop with 10 iterations, create div (card) in classlist card, append child to colmd3textcenter
     //constructor(region, season, weather, vacationType){
-    constructor(){
+    constructor(destinations) {
         this.region = "region";
         this.season = "season";
         this.weather = "weather";
         this.vacationType = "vacationType";
-        this.destinations = [];
+        this.destinations = destinations;
     }
 
-    populate(list){
+    populate(list) {
         // create a list of objects = destinations from json file where region = region....
         //this will populate this.destinations from the the json file where region= region, season= season...
-    
+
     }
-    addToFavorites(){
+    addToFavorites() {
         //this will call a function in database that will upload the destination to the 
         //database which will connect the destination to the user so the user can view it 
         //in their favoritues page
     }
 
-    render(element){
-        for(let i = 0; i<3; i++){
+    render(element) {
+        for (let i = 0; i < 3; i++) {
             console.log(i);
             const box = document.createElement("div");
             box.classList.add("card");
@@ -38,7 +38,7 @@ export class SearchResults{
 
             const body = document.createElement("div");
             body.classList.add("card-body");
-            
+
             //add pic of element at index 0 in destinations list
             const pic = document.createElement("img");
             //pic.src = "images/cs326_proj1_ftLaud.jpg";
@@ -53,7 +53,7 @@ export class SearchResults{
             box.appendChild(place);
             box.appendChild(body);
             // box.appendChild(button);
-            
+
             element.appendChild(box);
         }
     }
