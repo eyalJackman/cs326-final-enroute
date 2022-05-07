39c1ae972fc4224e664c1246fc648c702c9bbcc5
eyalJackman <eyal.jackman@gmail.com>
2022-05-07 09:47:55 -0700

    Minor fixes

--- client/login.js
+++ client/login.js
@@ -12,12 +12,12 @@ const user_information = {};
 
 async function userRequestFetch(username, password) {
   const data = JSON.stringify({ username, password });
-  const response = await fetch('/checkUser', {
-    method: 'POST',
+  const response = await fetch("/checkUser", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: data
+    body: data,
   });
   if (!response.ok) {
     console.error(`Unable to check ${data}`);
@@ -27,30 +27,31 @@ async function userRequestFetch(username, password) {
 }
 
 let loginButton = document.getElementById("login_button");
-
-loginButton.addEventListener("click", async () => {
-  console.log("login button clicked");
-  let username = document.getElementById("login_username");
-  let password = document.getElementById("login_password");
-  if (username.value === "" || password.value === "") {
-    alert(`Please Add a Username and Password`);
-    return;
-  } else {
-    let userRequest = await userRequestFetch(username.value, password.value);
-    if (userRequest) {
-      username.value = "";
-      password.value = "";
-      // alert(`Your Are Logged In!`);
-      login = true;
-      user_information["id"] = userRequest["_id"];
-      user_information["username"]= userRequest["username"];
-      user_information["password"] = userRequest["password"];
+if (loginButton) {
+  loginButton.addEventListener("click", async () => {
+    console.log("login button clicked");
+    let username = document.getElementById("login_username");
+    let password = document.getElementById("login_password");
+    if (username.value === "" || password.value === "") {
+      alert(`Please Add a Username and Password`);
       return;
     } else {
-      alert(`Incorrect Account Entered`);
-      return;
+      let userRequest = await userRequestFetch(username.value, password.value);
+      if (userRequest) {
+        username.value = "";
+        password.value = "";
+        // alert(`Your Are Logged In!`);
+        login = true;
+        user_information["id"] = userRequest["_id"];
+        user_information["username"] = userRequest["username"];
+        user_information["password"] = userRequest["password"];
+        return;
+      } else {
+        alert(`Incorrect Account Entered`);
+        return;
+      }
     }
-  }
-});
+  });
+}
 
 export { login, user_information };
