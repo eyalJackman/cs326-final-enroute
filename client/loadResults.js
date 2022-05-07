c09e02a817ad6f356a79ff029970f8507fb4aa3d
eyalJackman <eyal.jackman@gmail.com>
2022-05-07 12:35:30 -0700

    Fixed render issue

--- client/loadResults.js
+++ client/loadResults.js
@@ -2,16 +2,13 @@ import { SearchResults } from "./searchResults.js";
 
 const destinations = JSON.parse(localStorage.getItem("destinations"));
 const resultsSection = document.getElementById("resultsContainer");
-resultsSection.appendChild(document.createTextNode("Test"));
+// resultsSection.appendChild(document.createTextNode("Test"));
 console.log(destinations);
 
 const loadDestinations = (destinations) => {
   const results = new SearchResults(destinations);
   console.log(results);
-  for (const destination in destinations) {
-    console.log("here");
-    results.render(resultsSection, destination);
-  }
+  results.render(resultsSection);
 };
 
 loadDestinations(destinations);
