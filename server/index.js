75c48ed771fd51e91e42a81e6f15f68957bb4c0a
eyalJackman <eyal.jackman@gmail.com>
2022-05-06 18:58:25 -0700

    Test

--- server/index.js
+++ server/index.js
@@ -2,87 +2,87 @@ import express from "express";
 import { Database } from "./database.js";
 import * as url from "url";
 
-class Server {
-  constructor(dburl) {
-    this.dburl = dburl;
-    this.app = express();
-    this.app.use(express.json());
-    this.app.use(express.static("client"));
-  }
+// class Server {
+//   constructor(dburl) {
+//     this.dburl = dburl;
+//     this.app = express();
+//     this.app.use(express.json());
+//     this.app.use(express.static("client"));
+//   }
 
-  async initRoutes() {
-    const self = this;
+//   async initRoutes() {
+//     const self = this;
 
-    this.app.post("/createUser", async (req, res) => {
-      try {
-        const { user, password } = req.body;
-        const account = await self.db.createUser(user, password);
-        res.send(JSON.stringify(account));
-      } catch (err) {
-        res.status(500).send(err);
-      }
-    });
+//     this.app.post("/createUser", async (req, res) => {
+//       try {
+//         const { user, password } = req.body;
+//         const account = await self.db.createUser(user, password);
+//         res.send(JSON.stringify(account));
+//       } catch (err) {
+//         res.status(500).send(err);
+//       }
+//     });
 
-    this.app.post("/checkUser", async (req, res) => {
-      try {
-        const { user, password } = req.body;
-        const validUser = await self.db.findUser(user, password);
-        res.send(validUser);
-      } catch (err) {
-        res.status(500).send(err);
-      }
-    });
+//     this.app.post("/checkUser", async (req, res) => {
+//       try {
+//         const { user, password } = req.body;
+//         const validUser = await self.db.findUser(user, password);
+//         res.send(validUser);
+//       } catch (err) {
+//         res.status(500).send(err);
+//       }
+//     });
 
-    this.app.post("/saveFilter", async (req, res) => {
-      try {
-        const { region, season, weather, vacation_type } = req.body;
-        const filter = await self.db.saveFilter(
-          region,
-          season,
-          weather,
-          vacation_type
-        );
-        res.send(JSON.stringify(filter));
-      } catch (err) {
-        res.status(500).send(err);
-      }
-    });
-    this.app.post("/getResults", async (req, res) => {
-      try {
-        const parsedURL = url.parse(req.url, true);
-        const { region, season, weather, vacation_type } = parsedURL.query;
-        const filter = await self.db.getResults(
-          region,
-          season,
-          weather,
-          vacation_type
-        );
-        // res.send(JSON.stringify(parsedURL.query));
-        res.send(JSON.stringify(filter));
-      } catch (err) {
-        res.status(500).send(err);
-      }
-    });
-  }
+//     this.app.post("/saveFilter", async (req, res) => {
+//       try {
+//         const { region, season, weather, vacation_type } = req.body;
+//         const filter = await self.db.saveFilter(
+//           region,
+//           season,
+//           weather,
+//           vacation_type
+//         );
+//         res.send(JSON.stringify(filter));
+//       } catch (err) {
+//         res.status(500).send(err);
+//       }
+//     });
+//     this.app.post("/getResults", async (req, res) => {
+//       try {
+//         const parsedURL = url.parse(req.url, true);
+//         const { region, season, weather, vacation_type } = parsedURL.query;
+//         const filter = await self.db.getResults(
+//           region,
+//           season,
+//           weather,
+//           vacation_type
+//         );
+//         // res.send(JSON.stringify(parsedURL.query));
+//         res.send(JSON.stringify(filter));
+//       } catch (err) {
+//         res.status(500).send(err);
+//       }
+//     });
+//   }
 
-  async initDb() {
-    this.db = new Database(this.dburl);
-    await this.db.connect();
-  }
+//   async initDb() {
+//     this.db = new Database(this.dburl);
+//     await this.db.connect();
+//   }
 
-  async start() {
-    await this.initRoutes();
-    await this.initDb();
-    const port = process.env.PORT || 8080;
-    this.app.listen(port, () => {
-      console.log(`Server listening on port ${port}!`);
-    });
-  }
-}
+//   async start() {
+//     await this.initRoutes();
+//     await this.initDb();
+//     const port = process.env.PORT || 8080;
+//     this.app.listen(port, () => {
+//       console.log(`Server listening on port ${port}!`);
+//     });
+//   }
+// }
 
-console.log(process.env.MONGODB_URI);
-const server = new Server(process.env.MONGODB_URI);
-server.start();
+// console.log(process.env.MONGODB_URI);
+// const server = new Server(process.env.MONGODB_URI);
+// server.start();
 
 // const TEMP_FILTER_FILE = "tempFilter.json";
 
@@ -112,42 +112,47 @@ server.start();
 // const saveFilter = saveToFilterFile(TEMP_FILTER_FILE);
 
 // // Create the Express app and set the port number.
-// const app = express();
-// const port = 3000;
+const app = express();
+const port = 3000;
 
-// // Add Middleware
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+// Add Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// //Add the morgan middleware to the app.
-// app.use(logger("dev"));
+//Add the morgan middleware to the app.
+app.use(logger("dev"));
 
-// // Add the express.static middleware to the app.
-// app.use(express.static("client"));
+// Add the express.static middleware to the app.
+app.use(express.static("client"));
 
-// app.get("/", (req, res) => {
-//     res.sendFile("client/home.html", { root: "./" });
+app.get("/", (req, res) => {
+  res.sendFile("client/home.html", { root: "./" });
+});
+
+// app.get("/client/destination", (req, res) => {
+//     res.send("Test");
+//     // const params = req.params;
+//     // const { name } = params;
+//     // await destinationView.addDestination(name); (Changes destination.html and then uploads it to the DOM)
+//     // res.sendFile("../client/destination.html");
 // });
-
-// // app.get("/client/destination", (req, res) => {
-// //     res.send("Test");
-// //     // const params = req.params;
-// //     // const { name } = params;
-// //     // await destinationView.addDestination(name); (Changes destination.html and then uploads it to the DOM)
-// //     // res.sendFile("../client/destination.html");
-// // });
 
 // app.post("/saveFilter", (req, res) => {
-//     const filter = req.body;
-//     saveFilter(filter.region, filter.season, filter.weather, filter.vacation_type);
-//     res.status(200).json({ status: "success" });
+//   const filter = req.body;
+//   saveFilter(
+//     filter.region,
+//     filter.season,
+//     filter.weather,
+//     filter.vacation_type
+//   );
+//   res.status(200).json({ status: "success" });
 // });
 
-// app.all("*", async(request, response) => {
-//     response.status(404).send(`Not found: ${request.path}`);
-// });
+app.all("*", async (request, response) => {
+  response.status(404).send(`Not found: ${request.path}`);
+});
 
-// // Start the server on port 3000.
-// app.listen(process.env.PORT || port, () => {
-//     console.log(`Server started on http://localhost:${port}`);
-// });
+// Start the server on port 3000.
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
