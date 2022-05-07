28cfeb4e068e34776800b590355030fa30bb598e
eyalJackman <eyal.jackman@gmail.com>
2022-05-07 12:35:19 -0700

    Updated destination view

--- client/destinationView.js
+++ client/destinationView.js
@@ -9,37 +9,42 @@ const name = document.getElementById("destination_name");
  * @returns void
  * @description This will render the first 100 words and an image of whatever destination is said to it. This will be sent to desination.html
  */
-const addDestination = async(destination) => {
-    // Image
-    const imgPath = `./images/${destination["image"]}`;
-    img.setAttribute("src", imgPath);
+const addDestination = async (destination) => {
+  // Image
+  console.log(typeof destination);
 
-    // Description
-    const correctFormat = destination["name"].split(" ").join("%20"); //Boston
-    const url = `https://en.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${correctFormat}`;
-    const response = await fetch(url);
+  const imgPath = destination["img"];
+  img.setAttribute("src", imgPath);
 
-    let extract;
-    await response
-        .json()
-        .then((obj) => {
-            const pages = obj["query"]["pages"];
-            for (const entry in pages) {
-                extract = pages[entry]["extract"];
-            }
-        })
-        .catch((err) => console.log(err));
-    if (response.ok) {
-        information.innerHTML = JSON.stringify(extract);
-    }
-    name.appendChild(document.createTextNode(destination["name"]));
+  // Description
+  console.log(destination["name"]);
+  const correctFormat = destination["name"].split(" ").join("%20"); //Boston
+  const url = `https://en.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${correctFormat}`;
+  const response = await fetch(url);
+
+  let extract;
+  await response
+    .json()
+    .then((obj) => {
+      const pages = obj["query"]["pages"];
+      for (const entry in pages) {
+        extract = pages[entry]["extract"];
+      }
+    })
+    .catch((err) => console.log(err));
+  if (response.ok) {
+    information.innerHTML = JSON.stringify(extract);
+  }
+  name.appendChild(document.createTextNode(destination["name"]));
 };
-const randDest = data[Math.floor(Math.random() * data.length)];
-addDestination(randDest);
-
+// const randDest = data[Math.floor(Math.random() * data.length)];
+// addDestination(randDest);
+const dest = localStorage.getItem("load");
+console.log(JSON.parse(dest));
+addDestination(JSON.parse(dest));
 /*
-    For now, it just works with destionation.html, showing random information from 3 places. 
-    This will be expanded to return an HTML file depending on the location, using the database later on.
-*/
+        For now, it just works with destionation.html, showing random information from 3 places. 
+        This will be expanded to return an HTML file depending on the location, using the database later on.
+    */
 
 export default addDestination;
